b8026847f4161ed06422a4244d8c90f2a5904edb
kyaw swar htet <kyawswarhtet101094@gmail.com>
2022-04-05 14:44:19 +0630

    update ksh

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-// const searchBox=document.getElementById('search');
-// const search_warp=document.getElementById("searchbox");
-// const alters=document.getElementById("alters");
-// const name=document.getElementById("name");
-// const navmeun= document.getElementById("menu_sub");
-// const submenu=document.querySelector("submenu");
+ const searchBox=document.getElementById('search');
+ const search_warp=document.getElementById("searchbox");
+ const alters=document.getElementById("alters");
+ const name=document.getElementById("name");
+ const navmeun= document.getElementById("menu_sub");
+ const submenu=document.querySelector("submenu");
 
-// searchBox.addEventListener('click',()=>{
-//     if(search_warp.style.display=='none'){
-//         search_warp.style.display='block';
-//     }else{
-//         search_warp.style.display='none';
-//     }
-// })
+ searchBox.addEventListener('click',()=>{
+     if(search_warp.style.display=='none'){
+         search_warp.style.display='block';
+     }else{
+         search_warp.style.display='none';
+     }
+ })
 
 
 
@@ -24,31 +24,31 @@
 
 
 // navigator bar
-// window.addEventListener('scroll', () => {
-//         document.querySelector('nav').classList.toggle
-//         ('window-scroll', window.scrollY > 0)
-//     })
+ window.addEventListener('scroll', () => {
+         document.querySelector('nav').classList.toggle
+         ('window-scroll', window.scrollY > 0)
+     })
 
 // show hide nav menu
 
-// const menu = document.querySelector(".nav_menu");
-// const menuBtn = document.querySelector("#open-menu-btn");
-// const closeBtn = document.querySelector("#close-menu-btn");
+ const menu = document.querySelector(".nav_menu");
+ const menuBtn = document.querySelector("#open-menu-btn");
+ const closeBtn = document.querySelector("#close-menu-btn");
 
-// menuBtn.addEventListener('click', () => {
-//     menu.style.display = "flex";
-//     closeBtn.style.display = "inline-block";
-//     menuBtn.style.display = "none";
-// })
+ menuBtn.addEventListener('click', () => {
+     menu.style.display = "flex";
+     closeBtn.style.display = "inline-block";
+     menuBtn.style.display = "none";
+ })
 
 // close nav menu
 
-// const closeNav = () => {
-//     menu.style.display = "none";
-//     closeBtn.style.display = "none";
-//     menuBtn.style.display = "inline-block";
-// }
-// closeBtn.addEventListener('click', closeNav);
+ const closeNav = () => {
+     menu.style.display = "none";
+     closeBtn.style.display = "none";
+     menuBtn.style.display = "inline-block";
+ }
+ closeBtn.addEventListener('click', closeNav);
 
 //show hidden product in makeup page
 const hiddenProduct = document.querySelectorAll('.hidden-container');
